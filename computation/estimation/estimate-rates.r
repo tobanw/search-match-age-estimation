9c25492477f119027309a290d4335cd2c5b32126
Toban Wiebe <tobanw@gmail.com>
2017-03-30 17:13:55 -0400

    second pass at rate estimation, still flawed

--- computation/estimation/estimate-rates.r
+++ computation/estimation/estimate-rates.r
@@ -16,158 +16,131 @@ db = dbConnect(RSQLite::SQLite(), 'acs_08-14.db')
 
 ### Categorization ###
 
-case_minority = 'case
-when "RACESING" in (1,4) and "HISPAN" = 0 then 0
-else 1
-end as MINORITY'
-case_minority_sp = 'case
-when "RACESING_SP" in (1,4) and "HISPAN_SP" = 0 then 0
-else 1
-end as MINORITY_SP'
-case_college = 'case
-when "EDUC" >= 10 then 1
-else 0
-end as COLLEGE'
-case_college_sp = 'case
-when "EDUC_SP" >= 10 then 1
-else 0
-end as COLLEGE_SP'
+case_minority = ' case when "RACESING" in (1,4) and "HISPAN" = 0 then 0 else 1 end '
+case_minority_sp = ' case when "RACESING_SP" in (1,4) and "HISPAN_SP" = 0 then 0 else 1 end '
+case_college = ' case when "EDUC" >= 10 then 1 else 0 end '
+case_college_sp = ' case when "EDUC_SP" >= 10 then 1 else 0 end '
 
 
-### Marriage rates ###
+### Queries ###
 
-# counts of new marriages by type pair
+# flows: counts of new marriages by type pair
 qry_marr_flow = paste0('select
-		"AGE_SP", ', case_college_sp, ', ', case_minority_sp,
-		', "AGE", ', case_college, ', ', case_minority,
-		', sum("HHWT") as MARFLOW
+		"AGE_SP" as AGE_M, ', case_college_sp, ' as COLLEGE_M, ', case_minority_sp, ' as MINORITY_M,
+		"AGE" as AGE_F, ', case_college, ' as COLLEGE_F, ', case_minority, ' as MINORITY_F,
+		sum("HHWT") as MARFLOW
 	from acs
-	where ("MARRINYR" = 2 and "MARST" <= 2)
+	where "MARRINYR" = 2 and "MARST" <= 2
 		and "SEX" = 2
-		and "AGE" >= 25
-	group by "AGE_SP", COLLEGE_SP, MINORITY_SP, "AGE", COLLEGE, MINORITY')
+		and AGE_F >= 25 and AGE_M >= 25
+	group by AGE_M, COLLEGE_M, MINORITY_M, AGE_F, COLLEGE_F, MINORITY_F')
 
-# counts of all marriages by type pair
-qry_marr_stock = paste0('select
-		"AGE_SP", ', case_college_sp, ', ', case_minority_sp,
-		', "AGE", ', case_college, ', ', case_minority,
-		', sum("HHWT") as MARSTOCK
+# stocks (lagged): counts of all marriages by type pair (by year for separating cohorts)
+qry_marr_stock = paste0('select "YEAR" + 1 as YEAR,
+		"AGE_SP" + 1 as AGE_M, ', case_college_sp, ' as COLLEGE_M, ', case_minority_sp, ' as MINORITY_M,
+		"AGE" + 1 as AGE_F, ', case_college, ' as COLLEGE_F, ', case_minority, ' as MINORITY_F,
+		sum("HHWT") as MARSTOCK
 	from acs
 	where "MARST" <= 2
 		and "SEX" = 2
-		and "AGE" >= 25
-	group by "AGE_SP", COLLEGE_SP, MINORITY_SP, "AGE", COLLEGE, MINORITY')
+		and AGE_F >= 25 and AGE_M >= 25
+	group by YEAR, AGE_M, COLLEGE_M, MINORITY_M, AGE_F, COLLEGE_F, MINORITY_F')
 
-# counts of singles eligible to marry
+# stocks of surviving marriages (by year for separating cohorts)
+qry_m1_stock = paste0('select "YEAR" as YEAR,
+		"AGE_SP" as AGE_M, ', case_college_sp, ' as COLLEGE_M, ', case_minority_sp, ' as MINORITY_M,
+		"AGE" as AGE_F, ', case_college, ' as COLLEGE_F, ', case_minority, ' as MINORITY_F,
+		sum("HHWT") as M1STOCK
+	from acs
+	where "MARST" <= 2 and "MARRINYR" != 2
+		and "SEX" = 2
+		and AGE_F >= 25 and AGE_M >= 25
+	group by YEAR, AGE_M, COLLEGE_M, MINORITY_M, AGE_F, COLLEGE_F, MINORITY_F')
+
+# counts of singles eligible to marry within the past year
 qry_men_elig = paste0('select
-		"AGE", ', case_college, ', ', case_minority,
-		', sum("PERWT") as SINGMEN
+		"AGE" as AGE_M, ', case_college, ' as COLLEGE_M, ', case_minority, ' as MINORITY_M,
+		sum("PERWT") as SNG_M
 	from acs
-	where "SEX" = 1
+	where "SEX" = 1 and AGE_M >= 25
 		and ("MARRINYR" = 2 or "MARST" >= 3)
-		and "AGE" >= 25
-	group by "AGE", COLLEGE, MINORITY')
+		and "DIVINYR" != 2
+	group by AGE_M, COLLEGE_M, MINORITY_M')
 qry_wom_elig = paste0('select
-		"AGE", ', case_college, ', ', case_minority,
-		', sum("PERWT") as SINGWOM
+		"AGE" as AGE_F, ', case_college, ' as COLLEGE_F, ', case_minority, ' as MINORITY_F,
+		sum("PERWT") as SNG_F
 	from acs
-	where "SEX" = 2
+	where "SEX" = 2 and AGE_F >= 25
 		and ("MARRINYR" = 2 or "MARST" >= 3)
-		and "AGE" >= 18
-	group by "AGE", COLLEGE, MINORITY')
-
-# population counts of men and women for weighting cells in regression
-qry_men_pop = paste0('select
-		"AGE", ', case_college, ', ', case_minority,
-		', sum("PERWT") as MENPOP
-	from acs
-	where "SEX" = 1 and "AGE" >= 25
-	group by "AGE", COLLEGE, MINORITY')
-qry_wom_pop = paste0('select
-		"AGE", ', case_college, ', ', case_minority,
-		', sum("PERWT") as WOMPOP
-	from acs
-	where "SEX" = 2 and "AGE" >= 25
-	group by "AGE", COLLEGE, MINORITY')
+		and "DIVINYR" != 2
+	group by AGE_F, COLLEGE_F, MINORITY_F')
 
 # queries return dataframes, convert to data.table
 master.dt = data.table(dbGetQuery(db, qry_marr_flow))
 mar.stock = data.table(dbGetQuery(db, qry_marr_stock))
+m1.stock = data.table(dbGetQuery(db, qry_m1_stock))
 
 men.elig = data.table(dbGetQuery(db, qry_men_elig))
 wom.elig = data.table(dbGetQuery(db, qry_wom_elig))
 
-men.pop = data.table(dbGetQuery(db, qry_men_pop))
-wom.pop = data.table(dbGetQuery(db, qry_wom_pop))
-
-# set keys for easy merging
-setkeyv(master.dt, colnames(master.dt)[1:6])
-setkeyv(mar.stock, colnames(mar.stock)[1:6])
-
-master.dt <- merge(master.dt, mar.stock, all=TRUE) # merge in the MARSTOCK column
-
-# merge in stocks of eligible singles (for computing rates from flows)
-master.dt <- merge(master.dt, men.elig,
-				   by.x=c("AGE_SP", "COLLEGE_SP", "MINORITY_SP"),
-				   by.y=c("AGE", "COLLEGE", "MINORITY")) # all=TRUE)
-master.dt <- merge(master.dt, wom.elig,
-				   by=c("AGE", "COLLEGE", "MINORITY")) # all=TRUE)
-
-# compute marriage rates
-master.dt[, MARRATE := MARFLOW / SINGMEN / SINGWOM] # sequential division to prevent integer overflow error
-
 
 ### Divorce rates ###
 
-master.dt[, MARSTOCK1 := MARSTOCK - MARFLOW] # marriages at least one year old
+div.dt <- merge(mar.stock, m1.stock,
+				by=c("YEAR", "AGE_M", "COLLEGE_M", "MINORITY_M", "AGE_F", "COLLEGE_F", "MINORITY_F"),
+				all=TRUE) # merge the MARSTOCK columns
+
+# master.dt is aggregate, div.dt is by cohort-year
+# need to compute DIVRATE by cohort and then average and merge into master.dt
 
 ## Death rates
 
 # prep mortality table for merging
 mort.dt[, DTHRT := DR100 / 100000] # convert individual probabilities
-mort.dt[, AGE := AGE_L + 5] # midpoints for interpolation: 30,40,50,60,70
+# lag by one year (5+1) so that death rate is for past year
+mort.dt[, AGE := AGE_L + 6] # midpoints for interpolation: 31,41,51,61,71
 mort.dt[AGE_H==24, AGE := 25] # boundaries for interpolation
 mort.dt[AGE_H==84, AGE := 79] # boundaries for interpolation
 
 # merge sparse mortality rates into full age table
-wom.pop <- merge(wom.pop, mort.dt[SEX==2, .(AGE, MINORITY, FDTHRT = DTHRT)],
-				   by=c("AGE", "MINORITY"), all=TRUE) # female 
-men.pop <- merge(men.pop, mort.dt[SEX==1, .(AGE, MINORITY, MDTHRT = DTHRT)],
-				   by=c("AGE", "MINORITY"), all=TRUE) # male 
+men.elig <- merge(men.elig, mort.dt[SEX==1, .(AGE_M = AGE, MINORITY_M = MINORITY, DTHRT_M = DTHRT)],
+				   by=c("AGE_M", "MINORITY_M"), all=TRUE) # male 
+wom.elig <- merge(wom.elig, mort.dt[SEX==2, .(AGE_F = AGE, MINORITY_F = MINORITY, DTHRT_F = DTHRT)],
+				   by=c("AGE_F", "MINORITY_F"), all=TRUE) # female 
 
 # interpolate death rates over ages within minority group
-men.pop[, MDTHRT := na.approx(MDTHRT, AGE), MINORITY] # male
-wom.pop[, FDTHRT := na.approx(FDTHRT, AGE), MINORITY] # female
+men.elig[, DTHRT_M := na.approx(DTHRT_M, AGE_M), MINORITY_M] # male
+wom.elig[, DTHRT_F := na.approx(DTHRT_F, AGE_F), MINORITY_F] # female
 
-# merge in population stocks and death rates
-master.dt <- merge(master.dt, men.pop,
-				   by.x=c("AGE_SP", "COLLEGE_SP", "MINORITY_SP"),
-				   by.y=c("AGE", "COLLEGE", "MINORITY"))
-master.dt <- merge(master.dt, wom.pop,
-				   by=c("AGE", "COLLEGE", "MINORITY"))
+# merge in death rates (and stocks of singles)
+div.dt <- merge(div.dt, men.elig, by=c("AGE_M", "COLLEGE_M", "MINORITY_M"))
+div.dt <- merge(div.dt, wom.elig, by=c("AGE_F", "COLLEGE_F", "MINORITY_F"))
 
 # separation rate due to deaths: assuming independence, which overestimates because of
 #  correlation between couple
-master.dt[, CPLDR := FDTHRT + MDTHRT - FDTHRT * MDTHRT]
+div.dt[, CPLDR := DTHRT_F + DTHRT_M - DTHRT_F * DTHRT_M]
 
-#FIXME: shift within couple types for BOTH ages!
-# death flows: product of younger cohort's stock and rate 
-master.dt[, DTHFLOW := shift(CPLDR) * shift(MARSTOCK)] # lag: initial year NA
-
-#FIXME: shift within couple types for BOTH ages!
 # divorce flows: total attrition less death flows
-master.dt[, DIVFLOW := MARSTOCK - shift(MARSTOCK1, type="lead") - DTHFLOW] # lead: final year NA
+#div.dt[, DIVFLOW := MARSTOCK * (1 - CPLDR) - M1STOCK]
 
-# compute divorce rates
-master.dt[, DIVRATE := DIVFLOW / MARSTOCK]
-# FIXME: this is way too big, almost all values are larger than 0.1 in abs!
+# divorce rates calculated per separate cohort
+div.dt[, DIVRTC := 1 - CPLDR - M1STOCK / MARSTOCK] # 1 - death rate - non-div rate
+
+# merge in the average divorce rate and total marriage stock (by cohort)
+#   as well as stocks of singles
+master.dt <- merge(master.dt,
+				   div.dt[, .(DIVRATE = mean(DIVRTC), WEIGHT = sum(MARSTOCK),
+							  SNG_M = SNG_M[1], SNG_F = SNG_F[1]),
+						  .(AGE_M, COLLEGE_M, MINORITY_M, AGE_F, COLLEGE_F, MINORITY_F)],
+				   by=c("AGE_M", "COLLEGE_M", "MINORITY_M", "AGE_F", "COLLEGE_F", "MINORITY_F"))
+
+
+### Marriage rates ###
+
+master.dt[, MARRATE := MARFLOW / SNG_M / SNG_F] # sequential division to prevent integer overflow error
 
 
 ### Weighted OLS ###
-
-# TODO: weight by MARSTOCK and drop obs with small values that give noisy DIVRATE
-# weight by pop size ℓm*ℓf, sample representativeness (as in GJR)
-master.dt[, WEIGHT := 1e-9 * MENPOP * WOMPOP] # shrink to prevent integer overflow error
 
 # drop NA rates, which may really bias the estimates
 reg.dt <- master.dt[!is.na(MARRATE) & !is.na(DIVRATE),
@@ -176,5 +149,4 @@ reg.dt <- master.dt[!is.na(MARRATE) & !is.na(DIVRATE),
 # weighted regression without an intercept
 ols.reg <- lm(Y ~ 0 + MARRATE + DIVRATE, data=reg.dt, weights=WEIGHT)
 
-# print results
-summary(ols.reg)
+#summary(ols.reg) to view results
